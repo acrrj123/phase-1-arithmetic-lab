da49781ac4270af7c3a4923b6412a754dc03c7d6
Anna Cole <acrrj123@gmail.com>
2023-04-07 12:49:03 -0500

    changes to pass test

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+const num1 = 62
+const num2 = 1
+const num3 = 16
+const num4 = 6
+
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10);
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 3, 20);
+
+
+
